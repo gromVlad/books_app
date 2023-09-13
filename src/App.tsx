0d9feb194b031f8a04c5f1ad5edd62c8e695fa6e
worh fr <128374434+gromVlad@users.noreply.github.com>
2023-09-13 16:21:15 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import styled from "styled-components";
 
 export function App() {
-
   return (
     <>
       <One />
+      <One />
     </>
-  )
+  );
 }
 
 const One = styled.input`
   background-color: red;
-`
-
+`;
