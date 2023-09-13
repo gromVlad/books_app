09317d88e684f55147368f7090cce562ca5ff6a3
worh fr <128374434+gromVlad@users.noreply.github.com>
2023-09-13 16:25:55 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,3 +12,5 @@ export function App() {
 const One = styled.input`
   background-color: red;
 `;
+
+//https://www.figma.com/file/iqDCVOyoQrBW7ejtPo3jCK/Ecommerce-Big-bookshelf-(Community)?type=design&node-id=659-2505&mode=design&t=wBrF1ooOWBP1RMWB-0
