7b4b729b5add6834cdb92f478cdef83b1122e50b
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-09-29 16:02:33 +0300

    comp. footer

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { thems } from "../../thems";
+import sprite from "../../../assets/sprite.svg";
+import { Typography } from "../typography/typography";
 
 type linkType = { name: string; link: string };
 type ArrDataLink = Array<linkType>;
@@ -10,13 +13,61 @@ type FooterType = {
 };
 
 export const Footer = ({ email, telephone, dataLink }: FooterType) => {
-
+  const tel = `${telephone}`
+  const em = `${email}`
 
   return (
     <>
-      <Container >
-        
-      </Container>
+      <ContainerUp>
+        <svg
+          width="166"
+          height="44"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <use xlinkHref={`${sprite}#${"logo"}`} />
+        </svg>
+        <div className="icons">
+          <a target="_blank" href={dataLink && dataLink[0].link}><svg
+            width="24"
+            height="25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <use xlinkHref={`${sprite}#${"insta"}`} />
+          </svg>
+        </a>
+          <a target="_blank" href={dataLink && dataLink[1].link}><svg
+            width="24"
+            height="25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <use xlinkHref={`${sprite}#${"fd"}`} />
+          </svg>
+        </a>
+          <a target="_blank" href={dataLink && dataLink[2].link}><svg
+            width="24"
+            height="25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <use xlinkHref={`${sprite}#${"tw"}`} />
+          </svg>
+        </a>
+        </div>
+
+      </ContainerUp>
+      <ContainerDown >
+        <div className="text">
+          <Typography variant="caption">Terms and conditions</Typography>
+          <Typography variant="caption">Privacy policy</Typography>
+        </div>
+        <div className="text">
+          {telephone ? <Typography variant="caption">{tel}</Typography> : ""}
+          {email ? <Typography variant="caption">{em}</Typography> : ""}
+        </div>
+      </ContainerDown>
     </>
   );
 };
@@ -24,6 +75,35 @@ export const Footer = ({ email, telephone, dataLink }: FooterType) => {
 
 
 
-const Container = styled.div`
-  
+const ContainerUp = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  width: 100%;
+  height: 88px;
+  padding: 0px 24px;
+  justify-content: space-between;
+  align-items: center;
+  background:${thems.colors.black.blackbg};
+
+  & .icons{
+    display: flex;
+    gap:16px;
+  }
+`
+
+const ContainerDown = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  width: 100%;
+  height: 40px;
+  padding: 12px 24px;
+  color:${thems.colors.white.whiteHighEmphasis};
+  justify-content: space-between;
+  align-items: center;
+  background:${thems.colors.black.blackHighEmpathis};
+
+  & .text{
+    display: flex;
+    gap:16px;
+  }
 `
